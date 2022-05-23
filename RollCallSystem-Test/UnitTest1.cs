8e03c42122cf1cbe5e040e450d59d450115bbd21
Kimon Kremizas <k.kremizas@gmail.com>
2022-05-23 20:21:13 +0200

    test sample

---
+++ RollCallSystem-Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RollCallSystem_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("test", "test");
+        }
+    }
+}
