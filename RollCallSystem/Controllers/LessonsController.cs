00226dcf1c92b1dadd96bd7a9a3496417419490f
jakubmraz <jakub.mraz111@gmail.com>
2022-06-06 18:33:50 +0200

    Removed unnecessary usings

--- RollCallSystem/Controllers/LessonsController.cs
+++ RollCallSystem/Controllers/LessonsController.cs
@@ -1,11 +1,6 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RollCallSystem.APIModels;
